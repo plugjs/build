8f2e40a01176e21506e96536a2e701b4331b81b7
Pier Fumagalli <pier@usrz.com>
2023-02-28 23:22:26 +0100

    As we are testing the build itself, we need to fork in the correct mode

--- build.ts
+++ build.ts
@@ -1,3 +1,20 @@
-import { tasks } from './src/index'
+import { build, exec, isDirectory, resolve, rmrf, tasks } from './src/index'
 
-export default tasks()
+export default build({
+  ...tasks(),
+
+  /** Run tests */
+  async test(): Promise<void> {
+    if (isDirectory(this.coverageDataDir)) await rmrf(this.coverageDataDir)
+
+    const [ node, cli ] = process.argv
+    const buildFile = resolve('./build.ts')
+    const args = [ node!, ...process.execArgv, cli!, '-f', buildFile ] as const
+
+    for (const type of [ 'cjs', 'esm' ]) {
+      await exec(...args, `--force-${type}`, `test_${type}`, {
+        coverageDir: '.coverage-data',
+      })
+    }
+  },
+})
