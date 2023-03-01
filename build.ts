04599bde8923f245995d15e2ced5bc8e1cb15b3e
Pier Fumagalli <pier@usrz.com>
2023-03-01 01:06:59 +0100

    New plug (with shared context) allow us not to fork!

--- build.ts
+++ build.ts
@@ -1,20 +1,3 @@
-import { build, exec, isDirectory, resolve, rmrf, tasks } from './src/index'
+import { tasks } from './src/index'
 
-export default build({
-  ...tasks(),
-
-  /** Run tests */
-  async test(): Promise<void> {
-    if (isDirectory(this.coverageDataDir)) await rmrf(this.coverageDataDir)
-
-    const [ node, cli ] = process.argv
-    const buildFile = resolve('./build.ts')
-    const args = [ node!, ...process.execArgv, cli!, '-f', buildFile ] as const
-
-    for (const type of [ 'cjs', 'esm' ]) {
-      await exec(...args, `--force-${type}`, `test_${type}`, {
-        coverageDir: '.coverage-data',
-      })
-    }
-  },
-})
+export default tasks()
