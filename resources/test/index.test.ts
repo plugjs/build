a158b544e9e5ab63f407d155ab8bed7919f3513d
Pier Fumagalli <pier@usrz.com>
2023-04-19 20:24:23 +0200

    Expect5 uses `toStrictlyEqual(...)` instead of `toBe(...)`

--- resources/test/index.test.ts
+++ resources/test/index.test.ts
@@ -2,6 +2,6 @@ import { theAnswer } from '../src/index'
 
 describe('Don\'t panic', () => {
   it('should provide the answer to life, the universe, and everything', () => {
-    expect(theAnswer).toBe(42)
+    expect(theAnswer).toStrictlyEqual(42)
   })
 })
