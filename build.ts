626f255045dc4eef70e8c35333b7ecce54ffb9fb
Pier Fumagalli <pier@usrz.com>
2023-08-28 11:00:56 +0200

    Ignore the `sample` directory in `test` while linting

--- build.ts
+++ build.ts
@@ -1,5 +1,20 @@
-import { tasks, logging } from './src/index'
+import { build, find, logging, merge, tasks } from './src/index'
+
+import type { Pipe } from './src/index'
 
 logging.logOptions.githubAnnotations = false
 
-export default tasks({ testGlob: 'build.test.ts' })
+export default build({
+  ...tasks({ testGlob: 'build.test.ts' }),
+
+  /** Override, as we want to ignore the whole test's `sample` directory */
+  _find_lint_sources(): Pipe {
+    return merge([
+      find('**/*.([cm])?ts', '**/*.([cm])?js', { directory: this.sourceDir }),
+      find('**/*.([cm])?ts', '**/*.([cm])?js', {
+        directory: this.testDir,
+        ignore: 'sample/**/*.*',
+      }),
+    ])
+  },
+})
